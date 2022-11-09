b1b7aca3fe762e2594ce2a7f3fa64fa35a83ba5b
mkrasnansk <miso.krasnansky@gmail.com>
2022-11-09 23:42:42 +0100

    sybmit

--- src/components/designer-portfolio/header/HeaderModal.jsx
+++ src/components/designer-portfolio/header/HeaderModal.jsx
@@ -1,12 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { SelectCotactForm } from "../../startup-agency/SelectCotactForm";
 
 const HeaderModal = () => {
+   const [name, setName] = useState("");
+   const [url, setUrl] = useState("owee.sk");
+   const [message, setMessage] = useState("");
+console.log(url);
+   const handleSubmitForm = async (e) => {
+      e.preventDefault();
+      console.log(name);
+      console.log(url);
+      try {
+         // "http://localhost:4000/mail"
+            // "http://backend.eba-r22udgp5.us-east-2.elasticbeanstalk.com/mail",
+            let res = await fetch( "http://backend.eba-r22udgp5.us-east-2.elasticbeanstalk.com/mail",{
+            method: "POST",
+            body: JSON.stringify({
+               url: url,
+               name: name,
+            }),
+            headers: new Headers({ "Content-Type": "application/json" })
+         });
+         console.log("ho");
+         let resJson = await res.json();
+         if (res.status === 200) {
+            console.log("ok");
+            setName("");
+            setMessage("Email send successfully");
+         } else {
+            setMessage("Some error occured");
+         }
+         console.log("success");
+      } catch (err) {
+         console.log(err);
+      }
+   };
    return (
       <div className="modal-content">
          <div className="modal-header bg-primary text-white">
             <div className="row align-items-center w-100">
                <div className="col">Contact Form</div>
+               <div className="col-auto ">
+               <div >{message ? message : null}</div>
+               </div>
                <div className="col-auto ">
                   <button type="button" className="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
                </div>
@@ -17,12 +53,12 @@ const HeaderModal = () => {
                <div className="row">
                   <div className="col-xl-11 m-auto">
                      <div className="user-data-form">
-                        <form>
+                        <form onSubmit={handleSubmitForm}>
                            <div className="row controls">
                               <div className="col-sm-6">
                                  <div className="input-group-meta form-group mb-25">
                                     <label>First Name*</label>
-                                    <input type="text" placeholder="First Name" name="name" required="required" data-error="Name is required." />
+                                    <input type="text" placeholder="First Name" name="name" onChange={(e) => setName(e.target.value)} data-error="Name is required." />
                                     <div className="help-block with-errors"></div>
                                  </div>
                               </div>
@@ -30,14 +66,14 @@ const HeaderModal = () => {
                               <div className="col-sm-6">
                                  <div className="input-group-meta form-group mb-25">
                                     <label>Last Name*</label>
-                                    <input type="text" placeholder="Last Name" name="Lname" required="required" />
+                                    <input type="text" placeholder="Last Name" name="Lname" />
                                  </div>
                               </div>
 
                               <div className="col-12">
                                  <div className="input-group-meta form-group mb-25">
                                     <label>Email*</label>
-                                    <input type="email" placeholder="owe@owe.com" name="email" required="required" />
+                                    <input type="email" placeholder="owe@owe.com" name="email" />
                                  </div>
                               </div>
                               <div className="col-12">
@@ -46,7 +82,7 @@ const HeaderModal = () => {
                               <div className="col-12">
                                  <label>Message</label>
                                  <div className="input-group-meta form-group mb-25 ">
-                                    <textarea className="form-control rounded-0 border-primary" rows={5} placeholder="Your message*" name="message" required="required"></textarea>
+                                    <textarea className="form-control rounded-0 border-primary" rows={5} placeholder="Your message*" name="message"></textarea>
                                  </div>
                               </div>
 
